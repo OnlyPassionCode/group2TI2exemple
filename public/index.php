fc6f70d2a2bf8e7d73f0c5af90a635c1c1fb2023
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-02-09 11:07:03 +0100

    Ajout bootstrap page simple

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+*   Font Controller / Chef d'orchestre
+*/
+
+/*
+*   Chargement des dépendances
+*/
+// chargement de configuration
+require_once "../config.php";
+
+try{
+    
+}catch(Exception $e){
+    die($e->getMessage());
+}
+
+include_once "../view/test.homepage.html.php";
